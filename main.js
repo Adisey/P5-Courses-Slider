34158caf9d05055b716a855381c402e11b4d8356
Adrey Smirnov <iadisey@gmail.com>
2018-04-06 21:58:00 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+
+var picturesLib = [
+    ["Slide 01", "http://adisey-ru.1gb.ru/photos/panorams/data/DSC4344_P-th1.jpg"],
+    ["Slide 02", "http://adisey-ru.1gb.ru/photos/panorams/data/dsc_0893_1000.jpg"],
+    ["Slide 03", "http://adisey-ru.1gb.ru/photos/panorams/data/DSC3044_Panorama-2.jpg"],
+    ["Slide 04", "http://adisey-ru.1gb.ru/photos/panorams/data/DSC4344_P-2000.jpg"],
+    ["Slide 05", "http://adisey-ru.1gb.ru/photos/panorams/data/p4_ps.jpg"],
+    ["Slide 06", "http://adisey-ru.1gb.ru/photos/panorams/data/img_1747_p-1200.jpg"],
+    ["Slide 07", "http://adisey-ru.1gb.ru/photos/panorams/data/img_1682-w.jpg"],
+    ["Slide 08", "http://adisey-ru.1gb.ru/photos/panorams/data/img_1337-p17.jpg"],
+    ["Slide 09", "http://adisey-ru.1gb.ru/photos/panorams/data/img_1326-30-3.jpg"],
+    ["Slide 10", "http://adisey-ru.1gb.ru/photos/panorams/data/img_1057_8_9-w.jpg"],
+    ["Slide 11", "http://adisey-ru.1gb.ru/photos/panorams/data/Smotruh1_w.jpg"],
+    ["Slide 12", "http://adisey-ru.1gb.ru/photos/panorams/data/kp01-w.jpg"],
+    ["Slide 13", "http://adisey-ru.1gb.ru/photos/panorams/data/p2-1-25_th500.jpg"],
+    ["Slide 14", "http://adisey-ru.1gb.ru/photos/panorams/data/p2-1-25.jpg"],
+    ["Slide 15", "http://adisey-ru.1gb.ru/photos/panorams/data/img_9079-p.jpg"],
+    ["Slide 16", "http://adisey-ru.1gb.ru/photos/panorams/data/p5-w4.jpg"]
+];
+
+$(document).ready(function () {
+    $("#but").click(function () {
+        console.log('111');
+        $("#currentPic").attr("src", "http://adisey-ru.1gb.ru/photos/panorams/data/img_1747_p-1200.jpg");
+        var aa = $('#currentPic');
+        console.log(aa);
+    });
+    
+    function preloadPic() {
+        
+    }
+    
+    
+    preloadPic();
+});
